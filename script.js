ed978d75793a8d2d8ec79b957bd45b9265531bff
sawliq <55914680+sawliq@users.noreply.github.com>
2020-11-16 23:31:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,133 @@
+// SELECT VARIOUS ELEMENTS
+const balanceEl = document.querySelector(".balance .value");
+const incomeTotalEl = document.querySelector(".income-total");
+const outcomeTotalEl = document.querySelector(".outcome-total");
+const incomeEl = document.querySelector("#income");
+const expenseEl = document.querySelector("#expense");
+const allEl = document.querySelector("#all");
+const incomeList = document.querySelector("#income .list");
+const expenseList = document.querySelector("#expense .list");
+const allList = document.querySelector("#all .list");
+
+// SELECT  BUTTONS
+const expenseBtn = document.querySelector(".tab1");
+const incomeBtn = document.querySelector(".tab2");
+const allBtn = document.querySelector(".tab3");
+
+// INPUT BTS
+ 
+//EXPENSE
+
+const addExpense = document.querySelector(".add-expense");
+const expenseTitle = document.getElementById("expense-title-input");
+const expenseAmount = document.getElementById("expense-amount-input");
+
+
+//INCOME 
+
+const addIncome = document.querySelector(".add-income");
+const incomeTitle = document.getElementById("income-title-input");
+const incomeAmount = document.getElementById("income-amount-input");
+
+// VARIABLES
+let ENTRY_LIST;
+let balance = 0, income = 0, outcome = 0;
+const DELETE = "delete", EDIT = "edit";
+
+
+
+// EVENT LISTENERS
+expenseBtn.addEventListener("click", function(){
+    show(expenseEl);
+    hide( [incomeEl, allEl] );
+    active( expenseBtn );
+    inactive( [incomeBtn, allBtn] );
+})
+incomeBtn.addEventListener("click", function(){
+    show(incomeEl);
+    hide( [expenseEl, allEl] );
+    active( incomeBtn );
+    inactive( [expenseBtn, allBtn] );
+})
+allBtn.addEventListener("click", function(){
+    show(allEl);
+    hide( [incomeEl, expenseEl] );
+    active( allBtn );
+    inactive( [incomeBtn, expenseBtn] );
+})
+
+addExpense.addEventListener("click", function(){
+    // IF ONE OF THE INPUTS IS LEFT EMPTY THEN SIMPLY EXIT
+    if(!expenseTitle.value || !expenseAmount.value ) return;
+
+    // SAVE THE ENTRY TO ENTRY_LIST
+    let expense = {
+        type : "expense",
+        title : expenseTitle.value,
+        amount :expenseAmount.value
+    }
+    ENTRY_LIST.push(expense);
+    
+
+    updateUI();
+    clearinput([expenseTitle.value,expenseAmount.value])
+   
+})
+
+addIncome.addEventListener("click", function(){
+    // IF ONE OF THE INPUTS IS EMPTY => EXIT
+    if(!incomeTitle.value || !incomeAmount.value ) return;
+
+    // SAVE THE ENTRY TO ENTRY_LIST
+    let income = {
+        type : "income",
+        title : incomeTitle.value,
+        amount : parseInt(incomeAmount.value)
+    }
+    ENTRY_LIST.push(income);
+
+    updateUI();
+    clearinput([incomeTitle.value,incomeAmount.value])
+})
+
+//Helping functions
+
+function updateUI(){
+
+    
+}
+
+
+
+function clearinput(inputs){
+    inputs.forEach(inputs=>{
+        inputs.value='';
+    })
+}
+
+
+
+
+
+function show(element){
+    element.classList.remove("hide");
+}
+
+function hide( elements ){
+    elements.forEach( element => {
+        element.classList.add("hide");
+    })
+}
+
+function active(element){
+    element.classList.add("active");
+}
+
+function inactive( elements )
+{
+    elements.forEach( element => {
+        element.classList.remove("active");
+    })
+}
+
+
